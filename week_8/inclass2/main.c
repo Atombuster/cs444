04ff56aad465fa7fe901dbb37f52cc5c6ffd3b0a
Austen Turbyne <austenturbyne@yahoo.com>
2025-05-21 12:13:37 -0700

    yay

--- week_8/inclass2/main.c
+++ week_8/inclass2/main.c
@@ -11,14 +11,19 @@
  */
 void ls(const char *dirname)
 {
-    // TODO
-    DIR *opendir(const char *name);
-    while (int something != NUll) {
-    something = *readdir(DIR *dirp);
-    printf("%d%c\n", something->d_ino, something->d_ino);
-    stat(name, something->d_ino);
+    char fullpath[4096];
+    struct dirent *something;
+    DIR *file = opendir(dirname);
+    struct stat statbuf;
+    while ((something = readdir(file)) != NULL) {
+        printf("Part 1: Inode number = %ld, Filename = %s\n\n", something->d_ino, something->d_name);
+        sprintf(fullpath, "%s/%s",dirname, something->d_name);
+        stat(fullpath, &statbuf);
+        //printf("%s\n", fullpath);
+        printf("Part 2: Filename = %s,\n Inode number = %ld,\n Octal = %o,\n User = %d,\n Group = %d,\n Size = %ld,\n Block Size = %ld,\n The number of 512-byte blocks allocated = %ld\n\n", something->d_name, something->d_ino, statbuf.st_mode, statbuf.st_uid, statbuf.st_gid, statbuf.st_size, statbuf.st_blksize, statbuf.st_blocks);
     }
     int closedir(DIR *dirp);
+    
 
 }
 
